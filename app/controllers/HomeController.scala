261afd80902d11ae1ca5a8f632ce4be1e2aa2c57
Nik <ni851ste@htwg-konstanz.de>
2019-10-22 15:50:28 +0200

    Nik switching to computer - delete this later

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,5 +1,8 @@
 package controllers
 
+import de.htwg.se.monopoly.Monopoly
+import de.htwg.se.monopoly.controller.Controller
+import de.htwg.se.monopoly.view.Tui
 import javax.inject._
 import play.api._
 import play.api.mvc._
@@ -11,6 +14,8 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+
+    var tui: Tui = null
     /**
      * Create an Action to render an HTML page.
      *
@@ -25,5 +30,19 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     def hello() = Action {
         Ok("WHY HELLO THERE")
     }
-    
+
+    def playGame()  = Action {
+
+        val controller = new Controller()
+        tui = new Tui(controller)
+
+        Ok("Game started")
+    }
+
+    def input(input: String) = Action {
+        tui.processInput(input)
+
+        Ok(input)
+    }
+
 }
