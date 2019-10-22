63c5bf17b37becae95ac2651d65e7c1de364b4dc
ni851ste <ni851ste@htwg-konstanz.de>
2019-10-22 21:15:59 +0200

    set up the server but the webpage doesnt handle the commands since it is stuck in while true loop

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,9 +2,9 @@ package controllers
 
 import de.htwg.se.monopoly.Monopoly
 import de.htwg.se.monopoly.controller.Controller
+import de.htwg.se.monopoly.util.Observer
 import de.htwg.se.monopoly.view.Tui
 import javax.inject._
-import play.api._
 import play.api.mvc._
 
 /**
@@ -14,35 +14,27 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
+    val controller: Controller = Monopoly.controller
+    val tui : Tui = Monopoly.tui
+    controller.add(new Observer {
+        override def update(): Unit = {
+            rummyAsString = controller.currentGameMessage()
+        }
+    })
+    var rummyAsString: String = ""
 
-    var tui: Tui = null
-    /**
-     * Create an Action to render an HTML page.
-     *
-     * The configuration in the `routes` file means that this method
-     * will be called when the application receives a `GET` request with
-     * a path of `/`.
-     */
+
     def index() = Action { implicit request: Request[AnyContent] =>
         Ok(views.html.index())
     }
 
-    def hello() = Action {
-        Ok("WHY HELLO THERE")
+    def startGame(): Action[AnyContent] = Action { implicit request: Request[AnyContent] =>
+        tui.processInput(request.getQueryString("input").getOrElse("ndkahbfhdfsdbfhm"))
+            Ok(rummyAsString)
     }
 
-    def playGame()  = Action {
 
-        val controller = new Controller()
-        tui = new Tui(controller)
 
-        Ok("Game started")
-    }
 
-    def input(input: String) = Action {
-        tui.processInput(input)
-
-        Ok(input)
-    }
 
 }
