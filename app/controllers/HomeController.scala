b6c61ac9f41010741ad4968a59b6f50fd5a03412
Nik <ni851ste@htwg-konstanz.de>
2019-10-22 14:36:51 +0200

    vanilla server works with jar

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,14 +11,19 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
+    /**
+     * Create an Action to render an HTML page.
+     *
+     * The configuration in the `routes` file means that this method
+     * will be called when the application receives a `GET` request with
+     * a path of `/`.
+     */
+    def index() = Action { implicit request: Request[AnyContent] =>
+        Ok(views.html.index())
+    }
+
+    def hello() = Action {
+        Ok("WHY HELLO THERE")
+    }
+    
 }
