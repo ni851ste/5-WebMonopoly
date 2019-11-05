e206abbd5c7a00ab5697c9481da92cbbeb6a69b3
ni851ste <ni851ste@htwg-konstanz.de>
2019-11-05 20:16:49 +0100

    divided css files, used Bootstrap

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,8 +11,8 @@ import play.api.mvc._
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-@Singleton
-class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
+@Singleton class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc)
+{
 
     val controller: Controller = new Controller()
     controller.controllerState = GameStatus.START_OF_TURN
@@ -20,49 +20,85 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     val tui: Tui = new Tui(controller)
 
     var monopolyAsString: String = ""
-    controller.add(new Observer {
-        override def update(): Unit = {
-            monopolyAsString = controller.getCurrentGameMessage()
-            Ok(monopolyAsString)
+    controller.add(new Observer
+    {
+        override def update(): Unit =
+        {
+            Ok(getCurrentMessageWeb())
         }
     })
 
-    def printState() = Action { implicit request: Request[AnyContent] =>
-        controller.notifyObservers()
-        Ok(views.html.game(controller))
+    def printState() = Action
+    {
+        implicit request: Request[AnyContent] =>
+            controller.notifyObservers()
+            Ok(views.html.game(controller, this))
     }
 
-    def printStateWithInput(input: String) = Action { implicit request: Request[AnyContent] =>
-        processInput(input)
-        controller.notifyObservers()
-        Ok(views.html.game(controller))
+    def printStateWithInput(input: String) = Action
+    {
+        implicit request: Request[AnyContent] =>
+            print("HAAAALP   " + controller.controllerState + "    " + input + "\n")
+            processInput(input)
+            controller.notifyObservers()
+            Ok(views.html.game(controller, this))
     }
 
-    private def processInput(input: String) = {
+    private def processInput(input: String) =
+    {
 
         if (input.equals("q")) System.exit(0)
 
-        controller.controllerState match {
-
-            case GameStatus.START_OF_TURN =>
-                input match {
-                    case "r" => val (d1, d2) = controller.rollDice()
-                        controller.processRoll(d1, d2)
-                }
+        print("\n############ " + controller.controllerState + "---" + input + "\n")
+        controller.controllerState match
+        {
+            case GameStatus.START_OF_TURN => input match
+            {
+                case "r" => val (d1, d2) = controller.rollDice()
+                    controller.processRoll(d1, d2)
+            }
 
             case GameStatus.CAN_BUILD =>
-                if (input.equals("e")) controller.nextPlayer()
-                val args = input.split("_")
-                if (args.length != 2) {
-                    controller.buildStatus = GameStatus.BuildStatus.INVALID_ARGS
+                if (input.equals("e"))
+                {
+                    controller.nextPlayer()
                 }
-                else {
+                else
+                {
+                    val args = input.split("_")
                     controller.tryToBuildHouses(args(0), args(1).toInt)
+
                 }
+
+            //                if (args.length != 2) {
+            //                    controller.buildStatus = GameStatus.BuildStatus.INVALID_ARGS
+            //                }
+            //                else {
+            //                }
         }
     }
 
-    def rules = Action { implicit request: Request[AnyContent] =>
-        Ok(views.html.rules());
+    def rules = Action
+    {
+        implicit request: Request[AnyContent] =>
+            Ok(views.html.rules());
+    }
+
+    def getCurrentMessageWeb(): String =
+    {
+        controller.controllerState match
+        {
+            case GameStatus.START_OF_TURN => "Start your turn by rolling the dice."
+            case GameStatus.CAN_BUILD =>
+                controller.buildStatus match
+                {
+                    case GameStatus.BuildStatus.DEFAULT =>
+                        "Rolled " + controller.currentDice + "\nNow on " + controller.getCurrentField.getName + ".\nYou can build houses."
+                    case GameStatus.BuildStatus.BUILT => "Succesfully build house"
+                }
+
+
+            case _ => "Unknown gamestate"
+        }
     }
 }
