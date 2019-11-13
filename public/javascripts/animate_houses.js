cc593c271bd5bfcc21389566a6fb9dbb0a88532f
ju851bah <julian@bahner.de>
2019-11-13 12:32:49 +0100

    Put JS libs into own directory

--- public/javascripts/animate_houses.js
+++ public/javascripts/animate_houses.js
@@ -1,16 +1,22 @@
-//var houseWrapper = document.querySelector('.house-par');
-//textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='houseIcon'>$&</span>");
-
-var tl = anime.timeline({loop: false})
+var tl = anime.timeline({loop: false, duration: 300})
 tl.add({
     targets: '.house-par .house',
     rotate: {
-        value: 360,
-        duration: 2000
+        value: 360
     },
-    translateZ: 0,
-    duration: 500,
-    delay: function (el, i) {
-        return i * 25;
+    scale: {
+        value: 1.5,
+        easing: 'easeInOutQuart'
+    },
+    translateZ: 0
+}).add({
+    targets: '.house-par .house',
+    rotate: {
+        value: 360,
+        direction: 'reverse'
+    },
+    scale: {
+        value: 1,
+        easing: 'easeInOutQuart'
     }
 });
