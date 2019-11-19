e4963d7f430f0b1f1aa10aeaffc9e459cf5f55a1
Nik <ni851ste@htwg-konstanz.de>
2019-11-19 18:20:19 +0100

    prepared transition to jquery

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,8 +11,7 @@ import play.api.mvc._
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
-@Singleton class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc)
-{
+@Singleton class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
     val controller: Controller = new Controller()
     controller.controllerState = GameStatus.START_OF_TURN
@@ -20,77 +19,63 @@ import play.api.mvc._
     val tui: Tui = new Tui(controller)
 
     var monopolyAsString: String = ""
-    controller.add(new Observer
-    {
-        override def update(): Unit =
-        {
+    controller.add(new Observer {
+        override def update(): Unit = {
             Ok(getCurrentMessageWeb())
         }
     })
 
-    def printState() = Action
-    {
+    def printState() = Action {
         implicit request: Request[AnyContent] =>
             controller.notifyObservers()
-            Ok(views.html.game(controller, this))
+            Ok(controller.getJSON())
     }
 
-    def printStateWithInput(input: String) = Action
-    {
+    def printStateWithInput(input: String) = Action {
         implicit request: Request[AnyContent] =>
             print("HAAAALP   " + controller.controllerState + "    " + input + "\n")
             processInput(input)
             controller.notifyObservers()
-            Ok(views.html.game(controller, this))
+            Ok(controller.getJSON())
     }
 
-    private def processInput(input: String) =
-    {
+    private def processInput(input: String) = {
 
         if (input.equals("q")) System.exit(0)
 
-        print("\n############ " + controller.controllerState + "---" + input + "\n")
-        controller.controllerState match
-        {
-            case GameStatus.START_OF_TURN => input match
-            {
+        print("\n############ " + controller.controllerState + " --- " + input + "\n")
+        controller.controllerState match {
+            case GameStatus.START_OF_TURN => input match {
                 case "r" => val (d1, d2) = controller.rollDice()
                     controller.processRoll(d1, d2)
             }
 
             case GameStatus.CAN_BUILD =>
-                if (input.equals("e"))
-                {
+                if (input.equals("e")) {
                     controller.nextPlayer()
                 }
-                else
-                {
+                else {
                     controller.tryToBuildHouses(input, 1)
 
                 }
-
-            //                if (args.length != 2) {
-            //                    controller.buildStatus = GameStatus.BuildStatus.INVALID_ARGS
-            //                }
-            //                else {
-            //                }
         }
+        getCurrentGameJson()
     }
 
-    def rules = Action
-    {
+    def rules = Action {
         implicit request: Request[AnyContent] =>
             Ok(views.html.rules());
     }
 
-    def getCurrentMessageWeb(): String =
-    {
-        controller.controllerState match
-        {
+    def getCurrentGameMessage(): Action[AnyContent] = Action {
+        Ok(getCurrentMessageWeb())
+    }
+
+    def getCurrentMessageWeb(): String = {
+        controller.controllerState match {
             case GameStatus.START_OF_TURN => "Start your turn by rolling the dice."
             case GameStatus.CAN_BUILD =>
-                controller.buildStatus match
-                {
+                controller.buildStatus match {
                     case GameStatus.BuildStatus.DEFAULT =>
                         "Rolled " + controller.currentDice + "\nNow on " + controller.getCurrentField.getName + ".\nYou can build houses."
                     case GameStatus.BuildStatus.BUILT => "Successfully build house"
@@ -101,7 +86,7 @@ import play.api.mvc._
         }
     }
 
-    def getCurrentGameJson() = {
-        controller.getJSON()
+    def getCurrentGameJson(): Action[AnyContent] = Action {
+        Ok(controller.getJSON())
     }
 }
